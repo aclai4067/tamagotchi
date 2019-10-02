860e42e8dee48d70cbf54403aab68b617d10e7d7
Ashley Claiborne <languageisaliquid@gmail.com>
2019-10-01 23:28:12 -0500

    setup complete

---
+++ src/javascripts/main.js
@@ -0,0 +1,8 @@
+import '@fortawesome/fontawesome-free/js/all';
+import '../styles/main.scss';
+
+const init = () => {
+  document.getElementById('pet').innerHTML = '<i class="far fa-moon"></i>';
+};
+
+init();
