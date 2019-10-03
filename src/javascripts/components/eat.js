62f615b17b09a87a05b937dc7b170a49f6490e6e
Ashley Claiborne <languageisaliquid@gmail.com>
2019-10-02 23:21:54 -0500

    added eat functionality

--- src/javascripts/components/eat.js
+++ src/javascripts/components/eat.js
@@ -1,7 +1,74 @@
 import utilities from '../helpers/utilities';
 
-const eat = () => {
-  utilities.printToDom('eat', 'test eat');
+let full = 100;
+const eatProgress = `
+  <div id='progressBar'>
+    <div id='eatBarPercent'></div>
+  </div>
+`;
+
+const eatArr = [
+  {
+    id: 'eat0',
+    icon: '<i class="eat0Btn fas fa-ice-cream"></i>',
+    points: -3,
+  },
+  {
+    id: 'eat1',
+    icon: '<i class="eat1Btn fas fa-apple-alt"></i>',
+    points: 10,
+  },
+  {
+    id: 'eat2',
+    icon: '<i class="eat2Btn fas fa-pizza-slice"></i>',
+    points: -3,
+  },
+  {
+    id: 'eat3',
+    icon: '<i class="eat3Btn fas fa-carrot"></i>',
+    points: 10,
+  },
+];
+
+const updateFullness = (e) => {
+  const selectedFood = e.target.classList.value;
+  const selectedFoodParent = e.target.parentNode.classList.value;
+  for (let n = 0; n < eatArr.length; n += 1) {
+    if (selectedFood.includes(eatArr[n].id) || selectedFoodParent.includes(eatArr[n].id)) {
+      full += eatArr[n].points;
+    }
+    if (full > 100) {
+      full = 100;
+    } else if (full < 0) {
+      full = 0;
+    }
+  }
+  const fullnessString = `${eatProgress} <div id='eatProgressPercent'>${full}%</div>`;
+  utilities.printToDom('fullScore', fullnessString);
+  document.getElementById('eatBarPercent').style.width = `${full}%`;
 };
 
-export default { eat };
+const printEatBtns = () => {
+  let eatString = `
+      <div class='row'>
+    `;
+  for (let i = 0; i < eatArr.length; i += 1) {
+    eatString += `
+      <button type='button' id='${eatArr[i].id}' class='${eatArr[i].id}Btn'>${eatArr[i].icon}</button>
+    `;
+  }
+  eatString += `
+      </div>
+      <div id='fullScore' class='row'>
+        ${eatProgress}
+        <div id='eatProgressPercent'>${full}%</div>
+      </div>
+      <p class='category snacks'>Eat</p>
+    `;
+  utilities.printToDom('eat', eatString);
+  for (let x = 0; x < eatArr.length; x += 1) {
+    document.getElementById(eatArr[x].id).addEventListener('click', updateFullness);
+  }
+};
+
+export default { printEatBtns };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,8 +3,7 @@ import '../styles/main.scss';
 import eat from './components/eat';
 
 const init = () => {
-  document.getElementById('pet').innerHTML = '<i class="far fa-moon"></i>';
-  eat.eat();
+  eat.printEatBtns();
 };
 
 init();
