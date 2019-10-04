52bbb2e7a4687a6250c7c41d6e81f26252d0079b
Ashley Claiborne <languageisaliquid@gmail.com>
2019-10-03 19:31:36 -0500

    added fight functionality

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,12 @@
 import '@fortawesome/fontawesome-free/js/all';
 import '../styles/main.scss';
 import eat from './components/eat';
+import fight from './components/fight';
 import play from './components/play';
 
 const init = () => {
   eat.printEatBtns();
+  fight.printFightBtns();
   play.printPlayBtns();
 };
 
