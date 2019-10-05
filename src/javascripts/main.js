bf39d3ccad24a0edf2fa7a371c6c37ffef762d7f
Ashley Claiborne <languageisaliquid@gmail.com>
2019-10-05 14:00:24 -0500

    WIP

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,7 +2,7 @@ import '@fortawesome/fontawesome-free/js/all';
 import '../styles/main.scss';
 import eat from './components/eat';
 import fight from './components/fight';
-import pet from './components/pet';
+import pet from './components/pet/pet';
 import play from './components/play';
 import sleep from './components/sleep';
 
